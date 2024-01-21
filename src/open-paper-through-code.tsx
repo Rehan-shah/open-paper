0eff1a976e42a9ffaf5a48aff4ca6f51922a7bb5
Rehan-shah <Rehansnehalshah1@gmail.com>
2024-01-21 21:55:33 +0530

    refactor: removing reduant vars in code selection file

--- src/open-paper-through-code.tsx
+++ src/open-paper-through-code.tsx
@@ -2,60 +2,60 @@ import { ActionPanel, Action, Form } from "@raycast/api";
 import { useState } from "react";
 
 
-type Cat = "s" | "m"|"w"
+type Cat = "s" | "m" | "w"
 
 type CodeStruct = {
-    subjectCode : number,
-    cat : Cat , 
-    year : number ,
-    varienat : number 
+    subjectCode: number,
+    cat: Cat,
+    year: number,
+    varienat: number
 }
 
 export default function Command() {
-    const [code , setCode]  = useState("")
-    const [error , setEroor] = useState<undefined | string>()
-    const [codeStruct , setCodeStruct] = useState<undefined | CodeStruct>()
+    const [code, setCode] = useState("")
+    const [error, setEroor] = useState<undefined | string>()
+    const [codeStruct, setCodeStruct] = useState<undefined | CodeStruct>()
 
 
     const checkIfValid = () => {
-        const arr = code.trim().split("/") 
-        let mainStruct : CodeStruct = {
-              subjectCode : 0,
-              cat : "s",
-              year : 0,
-              varienat : 0 
-            }
-        if (arr.length !== 5){
-            setEroor("invalid string") 
+        const arr = code.trim().split("/")
+        const mainStruct: CodeStruct = {
+            subjectCode: 0,
+            cat: "s",
+            year: 0,
+            varienat: 0
+        }
+        if (arr.length !== 5) {
+            setEroor("invalid string")
             return
         }
 
-        if (arr[0].length !== 4){
-            setEroor("invalid string") 
+        if (arr[0].length !== 4) {
+            setEroor("invalid string")
             return
         }
-        
+
         const val = parseInt(arr[0])
-        if ( isNaN(val) ) {
-            setEroor("invalid string") 
+        if (isNaN(val)) {
+            setEroor("invalid string")
             return
         }
         mainStruct.subjectCode = val
-        switch(arr[2].toLowerCase()) {
+        switch (arr[2].toLowerCase()) {
             case "f":
                 mainStruct.cat = "m"
                 break;
-            case "m" :
+            case "m":
                 mainStruct.cat = "s"
                 break;
-            case "o" :
+            case "o":
                 mainStruct.cat = "w"
                 break;
-            default :
-                setEroor("invalid string") 
-                return 
+            default:
+                setEroor("invalid string")
+                return
         }
-        if (arr[1].length != 2 && arr[4].length != 2){
+        if (arr[1].length !== 2 && arr[4].length !== 2) {
             setEroor("invalid string")
             return
         }
@@ -71,36 +71,34 @@ export default function Command() {
         setCodeStruct(mainStruct)
         return
     }
-    
-    function setString(val : string) {
-            setEroor(undefined)
-            setCode(val)
+
+    function setString(val: string) {
+        setEroor(undefined)
+        setCode(val)
     }
-   
-    function getUrl(type : "ms" | "qp") : string {
-        let str = `https://pastpapers.papacambridge.com/directories/CAIE/CAIE-pastpapers/upload/${codeStruct?.subjectCode}_${codeStruct?.cat}${codeStruct?.year}_${type}_${codeStruct?.varienat}.pdf#view=FitH`
-        console.log(str)
-        return str
+
+    function getUrl(type: "ms" | "qp"): string {
+        return `https://pastpapers.papacambridge.com/directories/CAIE/CAIE-pastpapers/upload/${codeStruct?.subjectCode}_${codeStruct?.cat}${codeStruct?.year}_${type}_${codeStruct?.varienat}.pdf#view=FitH`
     }
 
     return (
-    <Form actions={
-          <ActionPanel title="Open past paper">
+        <Form actions={
+            <ActionPanel title="Open past paper">
                 <Action.OpenInBrowser title="Open question paper"
- url={getUrl("qp")} />
+                    url={getUrl("qp")} />
                 <Action.OpenInBrowser title="Open markscheme" url={getUrl("ms")} />
-          </ActionPanel>
+            </ActionPanel>
         }>
-      <Form.TextField
-        id="subjectFeild"
-        title="Full paper code"
-        error={error}
-        placeholder="Enter the paper code here"
-        onBlur={checkIfValid}
-        onChange={setString}
-      />
-    </Form>
+            <Form.TextField
+                id="subjectFeild"
+                title="Full paper code"
+                error={error}
+                placeholder="Enter the paper code here"
+                onBlur={checkIfValid}
+                onChange={setString}
+            />
+        </Form>
     )
 
-    }
+}
 
